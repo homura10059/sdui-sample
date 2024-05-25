1e56f6036d22bd56dc3a0f7e2b10b396c506fa25
homura <preasper0+github@gmail.com>
2024-05-25 20:58:44 +0900

    add

--- sdui-web/src/app/page.tsx
+++ sdui-web/src/app/page.tsx
@@ -1,11 +1,23 @@
 import { client } from '@/lib/micro-cms-client'
-import Image from 'next/image'
 
-export default function Home() {
-  const sections = 'a'
+type Section = {
+  fieldId: 'section'
+  type: 'TITLE'
+  text: 'sample title'
+}
+
+type Sections = {
+  sections: Section[]
+}
+
+export default async function Home() {
+  const sections = await client.getObject<Sections>({ endpoint: 'sections' })
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      main
+      {sections.sections.map(section => {
+        /* biome-ignore lint/correctness/useJsxKeyInIterable: <explanation> */
+        return <h1>{section.text}</h1>
+      })}
     </main>
   )
 }
