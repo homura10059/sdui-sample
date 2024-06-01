1bfeddf419f016fe71648ce059e01959ae5481c7
homura <preasper0+github@gmail.com>
2024-06-01 20:58:44 +0900

    add

--- sdui-graphql-server/src/index.ts
+++ sdui-graphql-server/src/index.ts
@@ -1,56 +1,35 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { addMocksToSchema } from '@graphql-tools/mock'
+import { makeExecutableSchema } from '@graphql-tools/schema'
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
-];
+type Query {
+  hello: String
+  resolved: String
+}
+`
 
 const resolvers = {
-    Query: {
-        books: () => books,
-    },
-};
+  Query: {
+    resolved: () => 'Resolved'
+  }
+}
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
+const mocks = {
+  Int: () => 6,
+  Float: () => 22.1,
+  String: () => 'Hello'
+}
+
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
+  schema: addMocksToSchema({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    mocks,
+    preserveResolvers: true
+  })
+})
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-});
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } })
 
-console.log(`🚀  Server ready at: ${url}`);
+console.log(`🚀 Server listening at: ${url}`)
